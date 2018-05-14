5d3e1f7df8a4d3e9ccb974aa25d3156b96481709
RadiophonicOddity09 <charlesokwechime@gmail.com>
2018-05-14 15:51:43 +0000

    created an https get method which recieves a chunk of data

---
+++ chunks.js
@@ -0,0 +1,18 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf-8');
+    response.on('data', function(data) {
+      console.log("Chunks recieved: " + '/n' + data);
+    });
+  });
+
+}
+getAndPrintHTMLChunks();
